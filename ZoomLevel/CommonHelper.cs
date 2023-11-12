4d929138fbf61aea5f87e17a1da32003bffa4aaa
thespbgamer <30112696+thespbgamer@users.noreply.github.com>
2023-11-12 13:55:13 +0000

    Dependencies update & optimizations

---
+++ ZoomLevel/CommonHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using StardewModdingAPI;
+
+namespace ZoomLevel
+{
+    public partial class ModEntry
+    {
+        private class CommonHelper
+        {
+            internal static void RemoveObsoleteFiles(IMod mod, params string[] relativePaths)
+            {
+                string basePath = mod.Helper.DirectoryPath;
+
+                foreach (string relativePath in relativePaths)
+                {
+                    string fullPath = Path.Combine(basePath, relativePath);
+                    if (File.Exists(fullPath))
+                    {
+                        try
+                        {
+                            File.Delete(fullPath);
+                            mod.Monitor.Log($"Removed obsolete file '{relativePath}'.");
+                        }
+                        catch (Exception ex)
+                        {
+                            mod.Monitor.Log($"Failed deleting obsolete file '{relativePath}':\n{ex}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- ZoomLevel/ModEntry.cs
+++ ZoomLevel/ModEntry.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -8,7 +7,7 @@ using ZoomLevel.GenericModConfigMenu;
 
 namespace ZoomLevel
 {
-    public class ModEntry : Mod
+    public partial class ModEntry : Mod
     {
         private ModConfig configsForTheMod = new();
 
@@ -508,31 +507,6 @@ namespace ZoomLevel
 
                 UpdateZoomLevel(zoomLevelValue);
                 this.Monitor.Log(Helper.Translation.Get("consoleMessages.resetZoom.message", new { value = currentZoomLevel.ToString() }), LogLevel.Info);
-            }
-        }
-
-        private class CommonHelper
-        {
-            internal static void RemoveObsoleteFiles(IMod mod, params string[] relativePaths)
-            {
-                string basePath = mod.Helper.DirectoryPath;
-
-                foreach (string relativePath in relativePaths)
-                {
-                    string fullPath = Path.Combine(basePath, relativePath);
-                    if (File.Exists(fullPath))
-                    {
-                        try
-                        {
-                            File.Delete(fullPath);
-                            mod.Monitor.Log($"Removed obsolete file '{relativePath}'.");
-                        }
-                        catch (Exception ex)
-                        {
-                            mod.Monitor.Log($"Failed deleting obsolete file '{relativePath}':\n{ex}");
-                        }
-                    }
-                }
             }
         }
     }
